a6046ccaaf41fab72e823376f83bd3a8cddb9b74
Kaida Masaki <kaida.masaki@socrata.com>
2016-05-31 13:58:22 -0700

    End of day one

---
+++ src/reducers/reducer_books.jsx
@@ -0,0 +1,8 @@
+export default () => {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Harry Potter and the Deathly Hallows'},
+    { title: 'The Fellowship of the Ring'},
+    { title: 'The Art of Computer Programming Volume I' }
+  ];
+};
